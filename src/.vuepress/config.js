d3da19243216f1d1beb89a0cfbf00e41009a50c4
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-06 00:42:55 +0900

    Modify theme

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   title: "東京工科大学 田胡研究室",
   description: "東京工科大学 コンピュータサイエンス学部田胡研究室のページです。",
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/mplus1p.css' }]
+  ],
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
