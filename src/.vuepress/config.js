f7898a4b38f245c46ddbb6d88c358c2d5d91b11b
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-07 15:11:37 +0900

    Modify navbar items.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,7 +7,10 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: 'Top', link: '/' },
-      { text: '配属希望の方へ', link: '/for3rd/' },
+      { text: '研究室情報', items: [
+        { text: '配属希望の方へ', link: '/about/for3rd.html'},
+        { text: '技術スタック', link: '/about/technology-stack.html'}
+      ] },
       { text: 'プロジェクト', link: '/projects/' },
       { text: '連絡先', link: '/contact/' },
     ]
