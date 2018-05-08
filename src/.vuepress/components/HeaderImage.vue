deca6d76553a4674c14b0d35c898394a685ee8c7
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-08 18:51:45 +0900

    Add style to header image.

--- src/.vuepress/components/HeaderImage.vue
+++ src/.vuepress/components/HeaderImage.vue
@@ -3,15 +3,37 @@
     <img src="/header.png" alt="header image">
   </div>
 </template>
-<style scoped>
-.header-container {
-  margin-right: calc(50% - 50vw);
-  margin-left: calc(50% - 50vw);
-  margin-top: 0;
-}
+<style lang="stylus" scoped>
+/*---------------------------------------------
+* breakpoints
+*---------------------------------------------*/
+screen-sm = 768px
+screen-md = 992px
+screen-lg = 1200px
 
-img {
-  max-width: 100%;
-}
+breakpoint(bpName)
+    for name in bpName
+        if name == 'sm'
+            @media only screen and (min-width: screen-sm)
+                {block}
+        if name == 'md'
+            @media only screen and (min-width: screen-md)
+                {block}
+        if name == 'lg'
+            @media only screen and (min-width: screen-lg)
+                {block}
+        if name == 'sm-up'
+            @media only screen and (max-width: screen-sm)
+                {block}
+        if name == 'md-up'
+            @media only screen and (max-width: screen-md)
+                {block}
+        if name == 'lg-up'
+            @media only screen and (max-width: screen-lg)
+                {block}
+
+.header-container
+  +breakpoint(md-up)
+    margin-right calc(50% - 50vw)
+    margin-left calc(50% - 50vw)
 </style>
-
