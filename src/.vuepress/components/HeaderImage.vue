04ade84be635c27d938ed1c23e17c3542c76a330
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-08 18:09:41 +0900

    [WIP] Add header image to top page.

---
+++ src/.vuepress/components/HeaderImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header-container">
+    <img src="/header.png" alt="header image">
+  </div>
+</template>
+<style scoped>
+.header-container {
+  margin-right: calc(50% - 50vw);
+  margin-left: calc(50% - 50vw);
+  margin-top: 0;
+}
+
+img {
+  max-width: 100%;
+}
+</style>
+
